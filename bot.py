f5a718a281184afba07bb7e9197db535bf136898
z-sat <69357099+z-sat@users.noreply.github.com>
2020-08-15 21:10:13 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+TOKEN = 'token for this bot in https://discord.com/developers/applications within the "bot" tab'
+
+#this is the flag that our bot will respond too
+bot = commands.Bot(command_prefix='~')
+
+startup_extensions = ["music",]
+
+@bot.event
+async def on_ready():
+    print('Hello, world.')
+    
+    for extension in startup_extensions:
+        bot.load_extension(extension)
+        
+
+#connects to discord
+bot.run(TOKEN)
